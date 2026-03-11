328360540ff587eef9a1bd7f5b8125a10af13ec8
Vishal Jaiswal <kumardenzel@gmail.com>
2026-03-11 12:39:49 +0530

    refactor error handling in MongoDB connection to use ApiError class

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
+import ApiError from "../utils/ApiError.js";
 
 const connectDB = async () => {
     try {
-        if(!process.env.MONGO_URL) {
-            throw new Error("MONGO_URL is not defined in environment variables");
+        if(!process.env.MONGO_URL){
+            throw new ApiError("MONGO_URL is not defined in environment variables", 500, "connectDB");
         }
         await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
+        console.log("Connected to MongoDB");    
+    } catch (err) {
+        console.error("Failed to connect to MongoDB:", err);
+        throw new ApiError("Failed to connect to MongoDB", 500, "connectDB", err);
     }
 };
 
