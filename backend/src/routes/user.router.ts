fe91118d9f034451b287a4e8e7b33edb32452991
Vishal Jaiswal <kumardenzel@gmail.com>
2026-03-14 14:27:30 +0530

    add user authentication and content management endpoints

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { UserSignUp } from "../controllers/user.controller.js"; 
+import { AddContent, DeleteContent, GetAllContent, UserSignIn, UserSignOut, UserSignUp } from "../controllers/user.controller.js"; 
+import {authMiddleware} from "../middlewares/auth.middleware.js";
 const UserRouter=Router();
 
 UserRouter.post('/signup',UserSignUp);
+UserRouter.post('/sign-in',UserSignIn);
+UserRouter.post('/sign-out',authMiddleware,UserSignOut);
+UserRouter.post('/add-content',authMiddleware,AddContent);
+UserRouter.get('/get-all-content',authMiddleware,GetAllContent);
+UserRouter.delete('/delete-content/:contentId',DeleteContent);
+
 
 
 export default UserRouter;
