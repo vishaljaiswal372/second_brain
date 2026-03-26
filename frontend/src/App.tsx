c848fe65dceb4dd45f7e2c7a24b9ff6a836ccfce
Vishal Jaiswal <kumardenzel@gmail.com>
2026-03-26 23:56:33 +0530

    feat: add routing with react-router-dom, implement LeftSideBar component, and enhance icons for better UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,41 @@ import { Card } from './componenets/ui/Card'
 import { DeleteIcon } from './assets/DeleteIcon'
 import { AddContentBox } from './componenets/ui/addcontentbox'
 import { useState } from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { LeftSideBar } from './componenets/ui/leftsidebar'
+import { TweetIcon } from './assets/TweetIcon'
+import { YoutubeIcon } from './assets/YoutubeIcon'
 
 function App() {
   const [open,SetContentBox]=useState(false);
+
+  const LeftSideBarContent=[{
+    Icon:<TweetIcon/>,
+    text:"Tweets"
+  },{
+    Icon:<YoutubeIcon/>,
+    text:"Videos"
+  }]
+
   return (
-  
-    <div className='flex flex-col'>
-      <div><AddContentBox openBox={open} onClose={()=>{SetContentBox(false)}}/></div>
-      <div className='flex gap-2 m-2'>
-        <Button variant='secondary' text='Share Brain' startIcon={<ShareIcon />} />
-        <Button variant='primary' text='Add Content' startIcon={<AddIcon />} onClick={()=>{SetContentBox(false)}}/>
+  <>
+    <AddContentBox openBox={open} onClose={()=>{SetContentBox(false)}}/>
+    <div className='flex'>
+      <LeftSideBar arr={LeftSideBarContent}/>
+      <div className='flex flex-col w-[85%] mt-6'>
+      <div className='flex items-center justify-between'>
+        <div className='text-3xl font-bold'>All Notes</div>
+        <div className='flex gap-3 m-2'>
+          <Button variant='secondary' text='Share Brain' startIcon={<ShareIcon />} />
+          <Button variant='primary' text='Add Content' startIcon={<AddIcon />} onClick={()=>{SetContentBox(true)}}/>
+        </div>
       </div>
-      <div>
+      <div className='flex gap-3'>
         <Card heading='Twitter tweet' Type='twitter' leftIcon={<AddIcon/>} RightFirstIcon={<ShareIcon/>} RightSecondIcon={<DeleteIcon/>} tag={["productivity","notes"]}/>
       </div>
+      </div>
     </div>
+  </>
   )
 }
 
--- frontend/src/componenets/ui/leftsidebar.tsx
+++ frontend/src/componenets/ui/leftsidebar.tsx
@@ -1,4 +1,32 @@
+import type { ReactElement } from "react";
+import { BrainIcon } from "../../assets/BrainIcon";
 
-export const LeftSideBar=()=>{
-    
+interface LeftSideBarProps{
+    arr:{
+        Icon:ReactElement;
+        text:string;
+    }[];
+}
+
+export const LeftSideBar=(props:LeftSideBarProps)=>{
+    return (
+        <div className="flex flex-col w-[25%] gap-2">
+            <div className="flex items-center p-2.5">
+                <div><BrainIcon/></div>
+                <div className="text-[#000000] text-3xl font-bold">Second Brain</div>
+            </div>
+            <div className="flex flex-col justify-center p-2.5 ml-7 cursor-pointer">
+                {props.arr?.map((item, index) => (
+                    <div key={index} className="flex items-center gap-2">
+                        <div>{item.Icon}</div>
+                        <div className="font-bold relative w-fit group cursor-pointer">
+                            <span>{item.text}</span>
+                            <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-[#7a92c9] transition-all duration-300 group-hover:w-full"></span>
+                        </div>
+                        {/* <div className="font-bold hover:underline transition-all duration-1000">{item.text}</div> */}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 };
